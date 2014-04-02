31a24fcf69af4e6d96152d33ff310472f95866bd
web_gearbox <webgearbox@gmail.com>
2014-04-02 18:37:02 +0300

    IE8 support added.

--- splitter.js
+++ splitter.js
@@ -1,3 +1,8 @@
+function addEvent(element,event,handler){
+    if(element.addEventListener)element.addEventListener(event,handler);
+    else element.attachEvent('on'+event,handler);
+}
+
 HTMLCollection.prototype.filterByTagName = function(tagname){
 
     var filtered = [];
@@ -10,6 +15,20 @@ HTMLCollection.prototype.filterByTagName = function(tagname){
     return filtered;
 };
 
+if (typeof Array.prototype.forEach == 'undefined') {
+    Array.prototype.forEach = function (callback) {
+        for (var i = 0; i < this.length; i++) {
+            callback.apply(this, [this[i], i, this]);
+        }
+    };
+}
+
+function NodeListAsArray(nl){
+    var arr=[];
+    for(var i=-1,l=nl.length>>>0;++i!==l;arr[i]=nl[i]);
+    return arr;
+}
+
 Element.prototype.hasClass = function(className){
     // Replace with brutal ternary for sake of bad readability.
     if (this.classList)return this.classList.contains(className);
@@ -19,6 +38,7 @@ Element.prototype.hasClass = function(className){
 var splitMe = {
     currentElement : null,
     crutch:null,
+    resizes:[],
     triggerResizes : function(){
         for(var index in this.resizes)
             splitMe.update(this.resizes[index]);
@@ -49,7 +69,8 @@ var splitMe = {
             //splitMe.update(splitMe.currentElement);
             splitMe.triggerResizes();
 
-            event.preventDefault();
+            if(event)event.preventDefault();
+            return false;
         }
 
     },
@@ -85,8 +106,8 @@ var splitMe = {
         }
     },
     init : function(){
-        splitMe.resizes=Array.prototype.slice.call(document.getElementsByClassName('vertically_divided')).
-            concat(Array.prototype.slice.call(document.getElementsByClassName('horizontally_divided')));
+        splitMe.resizes=NodeListAsArray(document.querySelectorAll('.vertically_divided')).
+            concat(NodeListAsArray(document.querySelectorAll('.horizontally_divided')));
 
         splitMe.resizes.forEach(function(elem){
 
@@ -134,10 +155,11 @@ var splitMe = {
             divider.onmousedown = function(event){
                 elem.splitter.className += ' dragged';
                 splitMe.currentElement = elem;
-                event.preventDefault();
+                if(event)event.preventDefault();
+                return false;
             };
 
-            divider.addEventListener('touchstart',function(){
+            addEvent(divider,'touchstart',function(){
                 arert('Fuck you!');
             });
 
@@ -147,10 +169,10 @@ var splitMe = {
 
         })
 
-        window.addEventListener('mousemove',splitMe.move);
-        window.addEventListener('mouseup',splitMe.up);
+        addEvent(document,'mousemove',splitMe.move);
+        addEvent(document,'mouseup',splitMe.up);
 
-        window.addEventListener('resize',function(){
+        addEvent(window,'resize',function(){
             setTimeout(function(){
                 splitMe.triggerResizes();
             },20);
@@ -161,6 +183,7 @@ var splitMe = {
 };
 
 
-window.addEventListener('load',function(){
+
+addEvent(window,'load',function () {
     splitMe.init();
 });
